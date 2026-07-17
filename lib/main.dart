e7948f8087c27eb57fc5136cc87be19db76a597e
abjelinav1-wq <abjelinav@example.com>
2026-07-17 15:08:45 +0800

    added files for flutter

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              print("Clicked!");
+            },
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.pink[200],
+              child: Text("Click Me!"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
